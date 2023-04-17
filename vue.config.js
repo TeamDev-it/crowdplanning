fef7c68f4f71297da369e477c782506a0f679b36
Michele Giulietti <m.giulietti@teamdev.it>
2023-04-17 12:34:24 +0200

    vue.config for modularization

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,68 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const webpack = require("webpack");
+const path = require('path');
+
+console.log(__dirname);
+console.log("ENV:" + process.env.NODE_ENV);
+var isDevelopment = process.env.NODE_ENV == 'development';
+
+class ModuleIdPrefixer {
+  constructor(prefix) {
+    this.prefix = prefix;
+  }
+
+  apply(compiler) {
+    compiler.hooks.compilation.tap('ModuleIdPrefixer', (compilation) => {
+      compilation.hooks.afterOptimizeModuleIds.tap('ModuleIdPrefixer', (modules) => {
+        for (var mod of modules) {
+          if (mod.id && mod.id.match(/\.\/src\//))
+            mod.id = this.prefix + mod.id;
+        }
+      });
+    });
+  }
+}
+
+var plugins = [
+  new webpack.DefinePlugin({
+    "process.env": {
+      NODE_ENV: process.env.NODE_ENV,
+      BASE_URL: process.env.BASE_URL
+    }
+  }),
+  new ModuleIdPrefixer("module:crowdplanning")
+];
+
+module.exports = {
+  filenameHashing: false,
+  productionSourceMap: true,
+  outputDir: path.resolve(__dirname, isDevelopment ? "../../main/public/modules/crowdplanning" : 'dist'),
+  publicPath: "/modules/crowdplanning/",
+  configureWebpack: {
+
+    devServer: {
+      watchOptions: {
+        ignored: [/node_modules/, /public\/libs/, /libs/],
+      }
+    },
+    output: {
+      hashFunction: "sha256",
+      filename: '[name].js',
+      chunkFilename: '[name].js',
+      libraryTarget: "umd"
+    },
+    plugins: plugins,
+    externals: ["vue", "vue-router"]
+  },
+  chainWebpack: config => {
+    if (process.env.NODE_ENV == 'production') {
+      config.module.rule('css').oneOf('vue').uses.delete('extract-css-loader');
+      config.module.rule('less').oneOf('vue').uses.delete('extract-css-loader');
+      config.module.rule('less').oneOf('normal').uses.delete('extract-css-loader');
+      config.module.rule('css').oneOf('vue').use('style-loader').before('css-loader').loader('style-loader').end();
+      config.module.rule('less').oneOf('vue').use('style-loader').before('css-loader').loader('style-loader').end();
+      config.module.rule('less').oneOf('normal').use('style-loader').before('css-loader').loader('style-loader').end();
+    }
+  },
+  devServer: { https: false },
+  runtimeCompiler: true,
+}
