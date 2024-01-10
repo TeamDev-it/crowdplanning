bb5721901304ee1896dd7fca06d79ef6387589be
Damiano Brunori <d.brunori@teamdev.it>
2024-01-10 13:06:11 +0100

    Fix build

--- src/services/statesService.ts
+++ src/services/statesService.ts
@@ -3,35 +3,35 @@ import { baseRestService } from "./baseRestService";
 import { store } from "@/store";
 
 class StatesService extends baseRestService {
-    constructor() {
-        super();
-        this.baseUrl = () => CONFIGURATION.PlansServiceUri;
-    }
+  constructor() {
+    super();
+    this.baseUrl = () => CONFIGURATION.PlansServiceUri;
+  }
 
-    public async getStates(group: server.Group): Promise<server.State[]> {
-        const result = await this.Get<server.State[]>(`/states/${group.workspaceId}`) || [];
+  public async getStates(group: server.Group): Promise<server.State[]> {
+    const result = await this.Get<server.State[]>(`/states/${group.workspaceId}`) || [];
 
-        store.actions.crowdplanning.setStates({ groupId: group.id, states: result });
+    store.actions.crowdplanning.setStates({ groupId: group.id, states: result });
 
-        return result;
-    }
+    return result;
+  }
 
-    public async setState(state: server.State, groupId: string): Promise<server.State | null> {
-        let result: server.State | null = null;
+  public async setState(state: server.State, groupId: string): Promise<server.State | null> {
+    let result: server.State | null = null;
 
-        if (!state.id)
-            result = await this.Post("/states", state);
-        else
-            result = await this.Put("/states", state);
+    if (!state.id)
+      result = await this.Post("/states", state);
+    else
+      result = await this.Put("/states", state);
 
-        store.actions.crowdplanning.setState({groupId: groupId, state: state});
+    store.actions.crowdplanning.setState({ groupId: groupId, state: state });
 
-        return result;
-    }
+    return result;
+  }
 
-    public async removeState(id: number): Promise<void> {
-        await this.Delete(`/states/${id}`);
-    }
+  public async removeState(id: number): Promise<void> {
+    await this.Delete(`/states/${id}`);
+  }
 }
 
 export const statesService = new StatesService();
