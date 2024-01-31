b5e4c99fd40c637800e2c3fc8d4b9b8b31d3f2e2
Damiano Brunori <d.brunori@teamdev.it>
2024-01-31 17:49:05 +0100

    Add recursive nested items in group list.

--- src/components/crowdplanningGroupsList/crowdplanningGroupsList.vue
+++ src/components/crowdplanningGroupsList/crowdplanningGroupsList.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="group-list">
-    <div class="item main" @click=setNullCategory :class="{ active: !selectedCategory }">
+    <div class="item main" @click="setNullCategory" :class="{ active: !selectedCategory }">
       <span class="text">{{ $t('plans.groups.see_all', 'Tutte le categorie') }}</span>
       <span class="icon" v-if="hasPermission('groups.cancreate')" @click="createGroup">
         <i class="ti ti-plus"></i>
       </span>
     </div>
-    <crowdplanning-groups-item v-for="group in rootGroup.children" :idx="0" :key="group.id" :value="group" :selectedCategory="selectedCategory" @selectedCategory="setSelectedCategory(group)" @changedGroup="changedGroup" />
+    <div class="childrens">
+      <crowdplanning-groups-item
+        v-for="group in rootGroup.children"
+        :idx="0"
+        :key="group.id"
+        :value="group"
+        :selectedCategory="selectedCategory"
+        @selectedCategory="setSelectedCategory"
+        @changedGroup="changedGroup"
+      />
+    </div>
   </div>
 </template>
 
